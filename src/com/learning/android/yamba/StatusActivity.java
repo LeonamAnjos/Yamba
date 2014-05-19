d11147e6e6bd2a3f0f8c85097eb3a842a1cb40fd
Leonam Anjos <leonam.ricardo@gmail.com>
2014-05-19 07:17:01 -0300

    Activity Status - Setted RelativeLayout - Added widgets 	- button 	- editText

--- src/com/learning/android/yamba/StatusActivity.java
+++ src/com/learning/android/yamba/StatusActivity.java
@@ -1,15 +1,9 @@
 package com.learning.android.yamba;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class StatusActivity extends ActionBarActivity {
 
@@ -17,11 +11,6 @@ public class StatusActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
@@ -42,23 +31,6 @@ public class StatusActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_status,
-					container, false);
-			return rootView;
-		}
 	}
 
 }

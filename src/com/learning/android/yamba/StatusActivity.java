b8819816a2a3d505269652a9c7ca8da2c85a9abd
Leonam Anjos <leonam.ricardo@gmail.com>
2014-05-20 23:20:20 -0300

    Adding Fragment Dynamically

--- src/com/learning/android/yamba/StatusActivity.java
+++ src/com/learning/android/yamba/StatusActivity.java
@@ -9,7 +9,17 @@ public class StatusActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.new_activity_status);
+		// setContentView(R.layout.new_activity_status);
+		
+		// check if this activity was created before
+		if (savedInstanceState == null) {
+			// create a fragment
+			StatusFragment fragment = new StatusFragment();
+			getFragmentManager()
+				.beginTransaction()
+				.add(android.R.id.content,  fragment, fragment.getClass().getSimpleName())
+				.commit();
+		}
 	}
 	
 	@Override

3cef091dd20c56c6cd09d47f4b0af5cd56a4094d
Leonam Anjos <leonam.ricardo@gmail.com>
2014-05-24 20:04:40 -0300

    Generating some data to test Android Content Provider

--- src/com/learning/android/yamba/RefreshService.java
+++ src/com/learning/android/yamba/RefreshService.java
@@ -1,8 +1,10 @@
 package com.learning.android.yamba;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
@@ -10,16 +12,10 @@ import android.util.Log;
 
 public class RefreshService extends IntentService {
 	
+	static final String TAG = "RefreshService";
+	
 	public RefreshService() {
 		super(TAG);
-	}
-
-	static final String TAG = "RefreshService";
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -27,22 +23,7 @@ public class RefreshService extends IntentService {
 		super.onCreate();
 		Log.d(TAG, "onCreated");
 	}
-	
-	
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.d(TAG, "onStarted");
-		return super.onStartCommand(intent, flags, startId);
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		Log.d(TAG, "onDestroyed");
-	}
-
-	
 	// Worker thread
 	@Override
 	protected void onHandleIntent(Intent intent) {
@@ -61,8 +42,42 @@ public class RefreshService extends IntentService {
 		Log.d(TAG, "out onHandleIntent");
 		
 		
+		DbHelper dbHelper = new DbHelper(this);
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		
+		// server connection
+		
+		//List<Status> timeline = new ArrayList<Status>();
+		values.clear();
+		values.put(StatusContract.Column.ID, 1);
+		values.put(StatusContract.Column.USER, "user name");
+		values.put(StatusContract.Column.MESSAGE, "message");
+		values.put(StatusContract.Column.CREATED_AT, "01/01/2013");
+		
+		db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+		
+		
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(TAG, "onBind");
+		return super.onBind(intent);
 	}
 	
-	
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.d(TAG, "onStarted");
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroyed");
+	}
+
 
 }
---
+++ src/com/learning/android/yamba/StatusEntity.java
@@ -0,0 +1,59 @@
+package com.learning.android.yamba;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StatusEntity {
+
+	public static String TAG = "StatusEntity";
+
+	public static List<StatusEntity> getSomeInstances(int size) {
+		ArrayList<StatusEntity> entities = new ArrayList<StatusEntity>();
+		
+		for (int i = 0; i < size; i++) {
+			entities.add(new StatusEntity(i+1, "user " + i, "message " + i, "01/" + i % 12 + "/2014" ));
+		}
+		
+		return entities;
+	}
+	
+	private int id;
+	private String user;
+	private String message;
+	private String created_at;
+	
+	public StatusEntity(int id, String user, String message, String createdAt) {
+		this.setId(id);
+		this.setUser(user);
+		this.setMessage(message);;
+		this.setCreatedAt(createdAt);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int value) {
+		this.id = value;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String value) {
+		this.user = value;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String value) {
+		this.message = value;
+	}
+	public String getCreatedAt() {
+		return created_at;
+	}
+	public void setCreatedAt(String value) {
+		this.created_at = value;
+	}
+
+}

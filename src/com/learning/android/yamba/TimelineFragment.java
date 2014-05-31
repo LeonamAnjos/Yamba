a8c9ac7048c41255c1d8727cd71c64e944263324
Leonam Anjos <leonam.ricardo@gmail.com>
2014-05-30 23:12:01 -0300

    Implemented a adapter to get data from status provider by another thread

--- src/com/learning/android/yamba/TimelineFragment.java
+++ src/com/learning/android/yamba/TimelineFragment.java
@@ -7,7 +7,9 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.util.Log;
+import android.view.View;
 
 public class TimelineFragment extends ListFragment implements LoaderCallbacks<Cursor> {
 	static final String TAG = "TimelineFragment";
@@ -25,11 +27,27 @@ public class TimelineFragment extends ListFragment implements LoaderCallbacks<Cu
 	private static final int LOADER_ID = 42;
 	private SimpleCursorAdapter mAdapter;
 	
+//	private static final ViewBinder VIEW_BINDER = new ViewBinder() {
+//		
+//		@Override
+//		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+//			long timestamp;
+//			
+//			// Custom binding
+//			switch (view.getId()) {
+//			case R.id.list_item_text_created_at:
+//				break; 
+//			}
+//		}
+//	}
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		
 		mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item, null, FROM, TO, 0);
+		
+//		mAdapter.setViewBinder(VIEW_BINDER);
 		
 		setListAdapter(mAdapter);
 		
